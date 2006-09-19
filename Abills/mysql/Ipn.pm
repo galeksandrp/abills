7d8372a9e95db1321aee5f0bc33a97521115d86a
abills <>
2006-09-19 08:50:18 +0000

    *** empty log message ***

--- Abills/mysql/Ipn.pm
+++ Abills/mysql/Ipn.pm
@@ -87,7 +87,8 @@ sub user_ips {
       LEFT JOIN bills cb ON (c.bill_id=cb.id)
       LEFT JOIN dv_main dv ON (u.uid=dv.uid)
       LEFT JOIN tarif_plans tp ON (tp.id=dv.tp_id)
-    WHERE u.id=calls.user_name;";
+    WHERE u.id=calls.user_name
+    and calls.nas_id='$DATA->{NAS_ID}';";
   }
   else {
   	$sql = "SELECT u.uid, calls.framed_ip_address, calls.user_name, 
@@ -99,7 +100,8 @@ sub user_ips {
     NULL,
     1
     FROM dv_calls calls, users u
-   WHERE u.id=calls.user_name;";
+   WHERE u.id=calls.user_name
+   and calls.nas_id='$DATA->{NAS_ID}';";
   }  
   
   
