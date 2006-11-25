fea45efa4831bcd50123d68b3c101475add4fdad
abills <>
2006-11-25 11:23:31 +0000

    *** empty log message ***

--- Abills/mysql/Dv_Sessions.pm
+++ Abills/mysql/Dv_Sessions.pm
@@ -224,33 +224,13 @@ sub online {
     
  	  $dub_logins{$line->[0]}++;
  	  $dub_ports{$line->[$nas_id_field]}{$line->[$port_id]}++;
+    
+    my @fields = ();
+    for(my $i=0; $i<=$#RES_FIELDS+13; $i++) {
+       push @fields, $line->[$i];
+     }
 
-    push( @{ $nas_sorted{"$line->[$nas_id_field]"} }, [ 
-      $line->[0], 
-      $line->[1], 
-      $line->[2], 
-      $line->[3], 
-      $line->[4], 
-      $line->[5], 
-      $line->[6], 
-      $line->[7], 
-      $line->[8], 
-      $line->[9], 
-      $line->[10], 
-      $line->[11], 
-      $line->[12], 
-      $line->[13], 
-      $line->[14], 
-      $line->[15], 
-      $line->[16], 
-      $line->[17], 
-      $line->[18], 
-      $line->[19], 
-      $line->[20], 
-      $line->[21], 
-      $line->[22], 
-      $line->[23], 
-      $line->[24]]);
+    push( @{ $nas_sorted{"$line->[$nas_id_field]"} }, [ @fields ]);
 
   }
  
