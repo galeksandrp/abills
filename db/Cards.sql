fddfb00563f6fabb6ec5875a72b1aa3258731844
abills <>
2006-12-18 08:28:53 +0000

    *** empty log message ***

--- db/Cards.sql
+++ db/Cards.sql
@@ -19,27 +19,10 @@ CREATE TABLE `cards_dillers` (
   UNIQUE KEY `name` (`name`)
 );
 
-CREATE TABLE `cards_payments` (
-  `serial` varchar(10) NOT NULL default '',
-  `number` int(11) unsigned zerofill NOT NULL default '00000000000',
-  `pin` varchar(20) NOT NULL default '',
-  `sum` double(15,3) unsigned NOT NULL default '0.000',
-  `status` tinyint(2) unsigned NOT NULL default '0',
-  `datetime` datetime NOT NULL default '0000-00-00 00:00:00',
-  `aid` smallint(6) unsigned NOT NULL default '0',
-  `expire` date NOT NULL default '0000-00-00',
-  `diller_id` smallint(6) unsigned NOT NULL default '0',
-  `diller_date` date NOT NULL default '0000-00-00',
-  UNIQUE KEY `pin` (`pin`),
-  UNIQUE KEY `serial` (`serial`,`number`),
-  KEY `diller_id` (`diller_id`)
-);
-
 
 CREATE TABLE `cards_users` (
-  `serial` int(11) unsigned zerofill NOT NULL auto_increment,
+  `number` int(11) unsigned zerofill NOT NULL default '00000000000',
   `login` varchar(20) NOT NULL default '',
-  `password` varchar(16) NOT NULL default '',
   `status` tinyint(2) unsigned NOT NULL default '0',
   `datetime` datetime NOT NULL default '0000-00-00 00:00:00',
   `aid` int(11) unsigned NOT NULL default '0',
@@ -48,8 +31,9 @@ CREATE TABLE `cards_users` (
   `diller_id` smallint(6) unsigned NOT NULL default '0',
   `diller_date` date NOT NULL default '0000-00-00',
   `sum` double(10,2) unsigned NOT NULL default '0.00',
-  PRIMARY KEY  (`serial`),
-  UNIQUE KEY `serial` (`serial`),
-  UNIQUE KEY `login` (`login`),
+  `serial` varchar(10) NOT NULL default '',
+  `pin` varchar(20) NOT NULL default '',
+  `uid` int(11) unsigned NOT NULL default '0',
+  UNIQUE KEY `serial` (`number`,`serial`),
   KEY `diller_id` (`diller_id`)
 );
