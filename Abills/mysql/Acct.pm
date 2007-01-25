19d2bfec28f19837cec04c2f0410389dc9829af1
abills <>
2007-01-25 12:25:40 +0000

    *** empty log message ***

--- Abills/mysql/Acct.pm
+++ Abills/mysql/Acct.pm
@@ -74,10 +74,12 @@ if ($RAD->{USER_NAME} =~ /(\d+):(\S+)/) {
 #Start
 if ($acct_status_type == 1) { 
 
-  $self->query($db, "SELECT count(*) FROM dv_calls 
+  $self->query($db, "SELECT count(user_name) FROM dv_calls 
     WHERE user_name='$RAD->{USER_NAME}' and acct_session_id='$RAD->{ACCT_SESSION_ID}';");
 
-  if ($self->{list}->[0] < 1) {
+  my $aaa = `echo "$self->{list}->[0]---- " > /tmp/12121`;
+
+  if ($self->{list}->[0]->[0] < 1) {
    #Get TP_ID
    $self->query($db, "SELECT dv.tp_id FROM (users u, dv_main dv)
      WHERE u.uid=dv.uid and u.id='$RAD->{USER_NAME}';");
