5f05d7848f584cc4a3df2c866d0216c88f265ff0
abills <>
2007-04-12 09:53:42 +0000

    *** empty log message ***

--- Abills/mysql/Dv_Sessions.pm
+++ Abills/mysql/Dv_Sessions.pm
@@ -608,7 +608,7 @@ sub prepaid_rest {
 	
 	#Get User TP and intervals
   $self->query($db, "select tt.id, i.begin, i.end, 
-    if(u.activate<>'0000-00-00', DATE_FORMAT(curdate(), '%Y-%m-01'), u.activate), 
+    if(u.activate<>'0000-00-00', u.activate, DATE_FORMAT(curdate(), '%Y-%m-01')), 
      tt.prepaid, 
     u.id, u.uid, dv.tp_id, tp.name
 
