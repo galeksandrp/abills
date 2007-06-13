cf7e854f56fb0f2e70cc9f045be873a59a7877d0
abills <>
2007-06-13 07:48:15 +0000

    *** empty log message ***

--- libexec/racct.pl
+++ libexec/racct.pl
@@ -244,7 +244,7 @@ if (-d $conf{extern_acct_dir}) {
     foreach my $file (@contents) {
       if (-x "$conf{extern_acct_dir}/$file" && -f "$conf{extern_acct_dir}/$file") {
         # ACCT_STATUS IP_ADDRESS NAS_PORT
-        $res = `$conf{extern_acct_dir}/$file $acct_status_type $RAD->{NAS_IP_ADDRESS} $RAD->{NAS_PORT} $nas->{NAS_TYPE}`;
+        $res = `$conf{extern_acct_dir}/$file $acct_status_type $RAD->{NAS_IP_ADDRESS} $RAD->{NAS_PORT} $nas->{NAS_TYPE} $RAD->{USER_NAME}`;
         log_print('LOG_DEBUG', "External accounting program '$conf{extern_acct_dir}' / '$file' pairs '$res'");
        }
      }
