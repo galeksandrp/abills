c66c6b1dbd4948c1209d09403628036d810866ae
abills <>
2012-02-29 08:11:29 +0000

    *** empty log message ***

--- Abills/mysql/Auth.pm
+++ Abills/mysql/Auth.pm
@@ -320,7 +320,7 @@ if ($self->{LOGINS} > 0) {
 #  	# Zap session with same CID
   	if ($line->[0] ne '' && $line->[0] eq $RAD->{CALLING_STATION_ID} 
   	   && $NAS->{NAS_TYPE} ne 'ipcad' 
-  	   && $acrtive_nas{$line->[2]} && $active_nas{$line->[2]} eq $line->[0]) {
+  	   && $active_nas{$line->[2]} && $active_nas{$line->[2]} eq $line->[0]) {
   		$self->query($db, "UPDATE dv_calls SET status=2 WHERE user_name='$RAD->{USER_NAME}' and CID='$RAD->{CALLING_STATION_ID}' and status <> 2;", 'do');
          $self->{IP}=$line->[1];
     	   $active_logins--;
