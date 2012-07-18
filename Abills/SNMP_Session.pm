34b044dfee08b6546832d98473a132809e522173
abills <>
2012-07-18 09:25:12 +0000

    *** empty log message ***

--- Abills/SNMP_Session.pm
+++ Abills/SNMP_Session.pm
@@ -136,7 +136,7 @@ BEGIN {
     $SNMP_Session::ipv6available = 0;
 
     if (eval {require Socket6;} &&
-	eval {require IO::Socket::INET6; IO::Socket::INET6->VERSION("1.26");}) {
+	eval {require IO::Socket::INET6; IO::Socket::INET6->VERSION("1.26") < 2.00;}) {
         Socket6->import(qw(inet_pton getaddrinfo pack_sockaddr_in6));
 	$ipv6_addr_len = length(pack_sockaddr_in6(161, inet_pton(AF_INET6(), "::1")));
 	$SNMP_Session::ipv6available = 1;
