948b649d4a477befaf80509c2de725aa784a794c
abills <>
2013-03-25 14:43:31 +0000

    *** empty log message ***

--- db/Rwizard.sql
+++ db/Rwizard.sql
@@ -1,12 +1,12 @@
-
-CREATE TABLE reports_wizard (
-  id int(11) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name varchar(100) not null default '',
-  comments text not null default '',
-  query text not null default '',
-  query_total text not null default '',
-  fields text not null default '',
-  date date not null default '0000-00-00',
-  aid smallint(11) unsigned NOT NULL default 0,
-  unique (name)
-) COMMENT 'Reports Wizard';
+CREATE TABLE `reports_wizard` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL DEFAULT '',
+  `comments` text NOT NULL,
+  `query` text NOT NULL,
+  `fields` text NOT NULL,
+  `date` date NOT NULL DEFAULT '0000-00-00',
+  `aid` smallint(11) unsigned NOT NULL DEFAULT '0',
+  `query_total` text NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) COMMENT='Reports Wizard';
