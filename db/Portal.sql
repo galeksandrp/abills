5d3d8e1f3da0733696a811920d5e70393429988f
abills <>
2014-12-23 14:53:43 +0000

    *** empty log message ***

--- db/Portal.sql
+++ db/Portal.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS `portal_articles` (
   `id` int(10) unsigned NOT NULL auto_increment,
-  `title` varchar(255) character set utf8 default NULL,
-  `short_description` text character set utf8 NOT NULL,
-  `content` text character set utf8,
-  `status` tinyint(1) default NULL,
+  `title` varchar(255) not null default '',
+  `short_description` text NOT NULL default '',
+  `content` text NOT NULL default '',
+  `status` tinyint(1) NOT NULL default 0,
   `on_main_page` tinyint(1) default '0',
   `date` datetime default NULL,
-  `portal_menu_id` int(10) unsigned NOT NULL,
+  `portal_menu_id` int(10) unsigned NOT NULL default 0,
   PRIMARY KEY  (`id`),
   KEY `fk_portal_content_portal_menu` (`portal_menu_id`)
 )COMMENT='information about article';
@@ -14,10 +14,10 @@ CREATE TABLE IF NOT EXISTS `portal_articles` (
 
 CREATE TABLE IF NOT EXISTS `portal_menu` (
   `id` int(10) unsigned NOT NULL auto_increment,
-  `name` varchar(45) character set utf8 default NULL,
-  `url` varchar(100) character set utf8 default NULL,
+  `name` varchar(45) not null default '',
+  `url` varchar(100) not null default '',
   `date` datetime default NULL,
-  `status` tinyint(1) default NULL,
+  `status` tinyint(1) not null default 0,
   PRIMARY KEY  (`id`)
 )COMMENT='information about menu' ;
 
