2e98fef9659d20c580ca792f96a8d8381ef9db60
abills <>
2015-02-23 11:35:09 +0000

    *** empty log message ***

--- Abills/mysql/Acct.pm
+++ Abills/mysql/Acct.pm
@@ -552,27 +552,10 @@ sub rt_billing {
     }
   );
 
-  #  my $a = `date >> /tmp/echoccc;
-  #   echo "
-  #   UID: $self->{UID},
-  #   SUM: $self->{SUM} / $self->{CALLS_SUM},
-  #   BILL_ID: $self->{BILL_ID},
-  #   TP: $self->{TARIF_PLAN},
-  #   TIME_TARRIF: $self->{TIME_TARIF},
-  #   TRAFF_TARRIF: $self->{TRAF_TARIF},
-  #   TIME INTERVAL ID: $Billing->{TI_ID}
-  #
-  #   DURATION: $RAD->{INTERIUM_ACCT_SESSION_TIME},
-  #   IN: $RAD->{INTERIUM_INBYTE},
-  #   OUT: $RAD->{INTERIUM_OUTBYTE},
-  #   IN2: $RAD->{INTERIUM_INBYTE1},
-  #   OUT2: $RAD->{INTERIUM_OUTBYTE1}
-  #   \n" >> /tmp/echoccc`;
-
   $self->query2("SELECT traffic_type FROM dv_log_intervals 
      WHERE acct_session_id='$RAD->{ACCT_SESSION_ID}' 
            AND interval_id='$Billing->{TI_ID}'
-           AND uid='$self->{UID}';"
+           AND uid='$self->{UID}' FOR UPDATE;"
   );
 
   my %intrval_traffic = ();
