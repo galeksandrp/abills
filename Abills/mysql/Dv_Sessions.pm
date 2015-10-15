b05493075d1fa4c45957e948bf1e6d46f212d54e
abills <>
2015-10-15 07:33:05 +0000

    *** empty log message ***

--- Abills/mysql/Dv_Sessions.pm
+++ Abills/mysql/Dv_Sessions.pm
@@ -341,7 +341,7 @@ sub online {
     $dub_logins{ $line->{user_name} }++ if ($line->{user_name});
     $dub_ports{ $line->{nas_id} }{ $line->{nas_port_id} }++ if ($line->{nas_port_id});
   #  $dub_ips{ $line->{nas_id} }{ $line->{client_ip} }++ if ($line->{client_ip});
-    $dub_ips{ $line->{nas_id} }{ $line->{client_ip} }++ if ($line->{client_ip} && ($line->{status}=1 || ($line->{status}>=3 && $line->{status}<11)) ;
+    $dub_ips{ $line->{nas_id} }{ $line->{client_ip} }++ if ($line->{client_ip} && ($line->{status}=1 || ($line->{status}>=3 && $line->{status}<11))) ;
   }
 
   $self->{dub_ports}  = \%dub_ports;
